1f25ba71138cdd54abb4cfce36a7c85af4c4c666
thenujaplayz685-ui <thenujaplayz685@gmail.com>
2025-11-03 20:15:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function changeBackground() {
+    //Change Background to dancing rick
+    document.body.style.backgroundImage ="url('5zbwfe.gif')"
+
+    //Hide the heding and the button
+    document.querySelector(".everything").style.display ="none"
+
+    //Play song
+    document.getElementById("song").play()
+}
